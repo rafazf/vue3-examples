8e4ea6b2493ec5ff0f1f15eee2934973fb30e564
rafazf <raelzapataf@gmail.com>
2024-07-28 13:10:36 -0500

    ready

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Contact from './components/Contact.vue'
+</script>
+
+<template>
+  <div>
+    <Contact></Contact>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-yellow-300 text-black p-2 rounded flex flex-col">
+    <h1>contact</h1>
+    <label for="name">Nombre</label>
+    <input v-model="text" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const text = ref('')
+</script>
+
+<style scoped></style>
